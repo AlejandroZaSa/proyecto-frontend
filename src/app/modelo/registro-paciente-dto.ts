e868544a4a807ef0a50520c0f64e2d02809663dd
AlejandroZasa <az0031456@gmail.com>
2023-10-30 12:17:11 -0500

    Creando el form de registro

---
+++ src/app/modelo/registro-paciente-dto.ts
@@ -0,0 +1,17 @@
+export class RegistroPacienteDTO {
+    
+    nombre: string = "";
+    cedula: string = "";
+    telefono: string = "";
+    email: string = "";
+    password: string = "";
+    confirmaPassword: string = "";
+    foto: string = "";
+    fechaNacimiento: string = "";
+    ciudad: string = "";
+    eps: number = 0;
+    tipoSangre: string = "";
+    alergias: string = "";
+   // lista:HorarioDTO[] = [];
+
+}
--- src/app/pagina/registro/registro.component.ts
+++ src/app/pagina/registro/registro.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RegistroPacienteDTO } from 'src/app/modelo/registro-paciente-dto';
 
 @Component({
   selector: 'app-registro',
@@ -6,5 +7,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent {
+
+  registroPacienteDTO: RegistroPacienteDTO;
+  ciudades: string[];
+  tipo_sangre: string[];
+  archivos!: FileList;
+
+  constructor() {
+    this.registroPacienteDTO = new RegistroPacienteDTO();
+    this.ciudades = [];
+    this.cargarCiudades();
+    this.tipo_sangre = [];
+    this.cargarTipoSangre();
+  }
+
+  public registrar() {
+    if (this.archivos != null && this.archivos.length > 0) {
+      console.log(this.registroPacienteDTO);
+    } else {
+      console.log("Debe cargar una foto");
+    }
+  }
+
+  public sonIguales(): boolean {
+    return this.registroPacienteDTO.password == this.registroPacienteDTO.confirmaPassword;
+  }
+
+  private cargarCiudades() {
+    this.ciudades.push("Armenia");
+    this.ciudades.push("Calarcá");
+    this.ciudades.push("Pereira");
+    this.ciudades.push("Manizales");
+    this.ciudades.push("Medellín");
+  }
+
+  private cargarTipoSangre() {
+    this.tipo_sangre.push("a+");
+    this.tipo_sangre.push("b+");
+    this.tipo_sangre.push("b-");
+    this.tipo_sangre.push("ab+");
+    this.tipo_sangre.push("o-");
+  }
+
+  public onFileChange(event: any) {
+    this.archivos = event.target.files;
+
+    if (event.target.files.length > 0) {
+      const files = event.target.files;
+      console.log(files);
+    }
+  }
+
 
 }
